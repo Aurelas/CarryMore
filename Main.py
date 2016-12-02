4507adab399c73d258b879e71c573fc316ec1be6
Mike <Mikethemantrobus@gmail.com>
2016-12-01 22:31:58 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,78 +1,103 @@
 from RiotApi import RiotAPI
 from Gamestate import Gamestate
 from QLearningAgent import QLearningAgent
+import string
 import json
 import operator
 import random
-from MDP import mdp
-##################
-# I think we are gonna try Q-learning with this
-#################
+
+
+def parse_champion_name(name):
+    new_name =name.translate(None,"'")
+    new_name = new_name.translate(None," ")
+    return new_name.lower()
+
+
 def main():
 
-    #API stuff
-    possible_picks = [1,2,3,4,5,6,7,7,9,10]
-    api = RiotAPI('RGAPI-91670249-d254-49f7-9036-cbb60b5bc523')
-    champs = 10
+    #Assuming that the user picks riven first for now
+    #Updates the gamestate and everything before we run it through a loop
 
-    stats = {'a': 100, 'b': 100, 'c':50}
+    ################################################################
+    # THIS IS WHERE USER INPUT SHOULD GO
 
-    print max(stats.iteritems(), key=operator.itemgetter(1))[0], "/////"
+    User_choice = parse_champion_name("""Miss Fortune""")
+    ################################################################
 
+    # USER COULD CHOOSE NUMBER OF SIMULATIONS?
+    #100000 simulations takes like 20 seconds to complete
+    # 1mil simulations takes ~ 1 - 1.5mins This should be the limit
+    simulations = 100000
 
-    #Agent Stuff
-
-    state = Gamestate()
-
-    Agent = QLearningAgent()
-    Agent.qValues[(1,2)] = 3
-
-
-    Agent2 = QLearningAgent()
-    Agent2.qValues[(1, 2)] = 8
-
-    state._ai.append(Agent)
-    state._ai.append(Agent2)
-
-    state.set_state(1)
-    ai = state.get_ai_from_state(state.get_state())
-    print ai.getQValue(1,2), "!!"
+    game = Gamestate()
+    #Depending on either True / False it swaps the reward function
+    User_choice_only = False
+    game.roles_picked[game.champion_roles[User_choice]] = True
+    game.update_champions(User_choice)
+    game.set_state(game.get_next_state())
 
 
+    #Creates AI for each champion type that we dont have after taking the user choice into consideration
+    for i in game.roles_picked.keys():
+        if not game.roles_picked[i]:
+            if i == 'ADC':
+               game.ai.append(QLearningAgent(game.adc_champs))
+            if i == 'Support':
+                game.ai.append(QLearningAgent(game.support_champs))
+            if i == 'Mid':
+                game.ai.append(QLearningAgent(game.mid_champs))
+            if i == 'Top':
+                game.ai.append(QLearningAgent(game.top_champs))
+            if i == 'Jungle':
+                game.ai.append(QLearningAgent(game.jungle_champs))
 
-    #Gamestate stuff
-    state = Gamestate()
+    #THE SIMULATOR
+    for i in range(0,simulations):
+        #Based on how many iterations are left in the simulation we make our ai choose less randomly
+        #if i <= simulations/3:
+            #for j in game.ai:
+                #j.epsilon = .1
+        #if i >= simulations/3 and i <= simulations/1.2:
+            #for j in game.ai:
+                #j.epsilon = .4
+        #if i >= simulations/1.2:
+            #for j in game.ai:
+                #j.epsilon = .7
 
-    while state.is_end_state() is False:
-        state.update_champions(random.randint(1,20000), state._state_num)
-        state.set_state(state._state_num + 1)
+        #For each simulation load up the champion list with picks
+        for i in game.ai:
+            game.update_champions(i.chooseChampion(game))
+            game.set_state(game.get_next_state())
+
+        j = 2
+        for i in game.ai:
+            #Only considers the User_choice synergies in reward function
+            if User_choice_only:
+                i.updateQValue(game,game.get_champion_from_state(j),
+                               i.altGetReward(game.get_champion_from_state(j),game))
+            #Considers all champions on the team for reward
+            else:
+                i.updateQValue(game, game.get_champion_from_state(j),
+                               i.getReward(game.get_champion_from_state(j), game))
+            j += 1
+
+        #Reset counters to keep track of game state
+        game.reset_champions_picked(User_choice)
+        if game.is_end_state():
+            game.set_state(1)
+
+    ################################################################
+    # THIS OUTPUTS THE TOP 5 CHOICES FOR EACH AI (ROLE)
+    ################################################################
+    for i in game.ai:
+        t = sorted(i.qValues.iteritems(), key=lambda x: -x[1])[:5]
+        for x , y in t:
+            print game.get_role_from_champion(x),":", x,":", y
 
 
-    print state._team1_champions_picked
-    print state._team2_champions_picked
-    print state._team2_champions_picked[0]
 
-    print state.get_champion_from_state(2)
-
-
-
-
-    summoner_response = api.get_summoner_by_name('KmancXC')
-
-    # Parse the output
-    temp = json.dumps(summoner_response)
-    temp = json.loads(temp)
-    print temp
-    summoner_id = (temp['kmancxc']['id'])
-    summoner_name = (temp['kmancxc']['name'])
-
-    mastery_response = api.get_top_mastery_data(summoner_id, champs)
-    print mastery_response
-    id = mastery_response[0]['championId']
-    print api.get_champion_role(summoner_id,[id]), "!!"
-
-    print id
 
 
 if __name__ == '__main__':
     main()
+
