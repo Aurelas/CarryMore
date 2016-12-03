fc3edb02e06abeddffda22b17e6547ecd9beb833
Joshua Semedo <joshsemedo@gmail.com>
2016-12-03 03:50:42 -0500

    i keep breaking things lmao

--- carrymore_results.php
+++ carrymore_results.php
@@ -10,8 +10,7 @@
 	<body>
 		<h1>Carrymore</h1>
 		<?php 
-			$champ_name = $_POST["champ"];
-			$s_champ_name = str_replace(" ", "", $champ_name);
+			$s_champ_name = str_replace(" ", "", $_POST["champ"]);
 			$sq_champ_name = str_replace("'", "", $s_champ_name);
 			$sqp_champ_name = str_replace(".", "", $sq_champ_name);
 			$clean = strtolower($sqp_clean_name);
