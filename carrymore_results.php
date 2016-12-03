6c65daf59cf59fb884d22bf2dbbfc9c1a3469935
Joshua Semedo <joshsemedo@gmail.com>
2016-12-03 04:21:43 -0500

    still testing

--- carrymore_results.php
+++ carrymore_results.php
@@ -22,56 +22,7 @@
 			$results = exec($command, $my_output, $status);
 			$data_array = explode(" , ", $results);
 			$champions = $data_array;
-			for ($i = 0; $i <= 4; $i++) {
-				if ($champion[i] == "aurelionsol") {
-					$champions[i] = "Aurelion Sol";
-				}
-				elseif ($champion[i] == "chogath"){
-					$champions[i] = "Cho'Gath";
-				}
-				elseif ($champion[i] == "drmundo"){
-					$champions[i] = "Dr. Mundo";
-				}
-				elseif ($champion[i] == "jarvaniv"){
-					$champions[i] = "Jarvan IV";
-				}
-				elseif ($champion[i] == "khazix"){
-					$champions[i] = "Kha'Zix";
-				}
-				elseif ($champion[i] == "kogmaw"){
-					$champions[i] = "Kog'Maw";
-				}
-				elseif ($champion[i] == "leesin"){
-					$champions[i] = "Lee Sin";
-				}
-				elseif ($champion[i] == "masteryi"){
-					$champions[i] = "Master Yi";
-				}
-				elseif ($champion[i] == "missfortune"){
-					$champions[i] = "Miss Fortune";
-				}
-				elseif ($champion[i] == "reksai"){
-					$champions[i] = "Rek'Sai";
-				}
-				elseif ($champion[i] == "aurelionsol"){
-					$champions[i] = "Aurelion Sol";
-				}
-				elseif ($champion[i] == "tahmkench"){
-					$champions[i] = "Tahm Kench";
-				}
-				elseif ($champion[i] == "twistedfate")
-					$champions[i] = "Twisted Fate";
-				}
-				elseif ($champion[i] == "velkoz"){
-					$champions[i] = "Vel'Koz";
-				}
-				elseif ($champion[i] == "xinzhao"){
-					$champions[i] = "Xin Zhao";
-				}
-				else {
-					#nothing
-				}
-			}
+
 		?>
 		<div class="outer">
 			<div class="inner">
