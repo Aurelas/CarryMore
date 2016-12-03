8f7c6e89e8436a4b475598f652ad4309a5408b6d
Joshua Semedo <joshsemedo@gmail.com>
2016-12-03 02:15:50 -0500

    attempting to clean up a bit of messy php

--- carrymore_results.php
+++ carrymore_results.php
@@ -10,14 +10,16 @@
 	<body>
 		<h1>Carrymore</h1>
 		<?php 
-
 			$champ_name = $_POST["champ"];
-			$ns_champ_name = str_replace(" ", "", $champ_name);
-			$cleaned_champ_name = str_replace("'", "", $ns_champ_name);
-			$lc_clean_name = strtolower($cleaned_champ_name);
+			function clean_name($string){
+				$string = str_replace(" ", "", $string);
+				$string = str_replace("'", "", $string);
+				$string = strtolower($string);
+			}
+			$clean_name = clean_name($champ_name);
 			$path = '/var/www/html/carrymore/Main.py';
 			$python = '/usr/bin/python';
-			$script = $python . " " . $path . " " . $champ_name;
+			$script = $python . " " . $path . " " . $clean_name;
 			$command = escapeshellcmd($script);
 			$results = exec($command, $my_output, $status);
 			$data_array = explode(" , ", $results);
@@ -26,47 +28,45 @@
 			$user_champ = $data_array[1];
 
 		?>
-			<div class="col-1"></div>
-			<div class="col-2" id="champ1" name="<?php echo $user_champ; ?>">
-				<p class="tileName">
-					<?php echo $user_champ; ?>
-				</p>
-				<p class="tileRole">
-					<?php echo $user_role; ?>
-				</p>
-			</div>
-			<div class="col-2" id="champ2" name="<?php echo $data_array[3]; ?>">
+		<div class="col-2" id="champ1" name="<?php echo $user_champ; ?>">
 			<p class="tileName">
-					<?php echo $data_array[3]; ?>
-				</p>
-				<p class="tileRole">
-					<?php echo $data_array[2]; ?>
-				</p>
-			</div>
-			<div class="col-2" id="champ3" name="<?php echo $data_array[5]; ?>">
-			<p class="tileName">
-					<?php echo $data_array[5]; ?>
-				</p>
-				<p class="tileRole">
-					<?php echo $data_array[4]; ?>
-				</p>
-			</div>
-			<div class="col-2" id="champ4" name="<?php echo $data_array[7]; ?>">
-			<p class="tileName">
-					<?php echo $data_array[7]; ?>
-				</p>
-				<p class="tileRole">
-					<?php echo $data_array[6]; ?>
-				</p>
-			</div>
-			<div class="col-2" id="champ5" name="<?php echo $data_array[9]; ?>">
-			<p class="tileName">
-					<?php echo $data_array[9]; ?>
-				</p>
-				<p class="tileRole">
-					<?php echo $data_array[8]; ?>
-				</p>
-			</div>
-			<div class="col-1"></div>
+				<?php echo $user_champ; ?>
+			</p>
+			<p class="tileRole">
+				<?php echo $user_role; ?>
+			</p>
+		</div>
+		<div class="col-2" id="champ2" name="<?php echo $data_array[3]; ?>">
+		<p class="tileName">
+				<?php echo $data_array[3]; ?>
+			</p>
+			<p class="tileRole">
+				<?php echo $data_array[2]; ?>
+			</p>
+		</div>
+		<div class="col-2" id="champ3" name="<?php echo $data_array[5]; ?>">
+		<p class="tileName">
+				<?php echo $data_array[5]; ?>
+			</p>
+			<p class="tileRole">
+				<?php echo $data_array[4]; ?>
+			</p>
+		</div>
+		<div class="col-2" id="champ4" name="<?php echo $data_array[7]; ?>">
+		<p class="tileName">
+				<?php echo $data_array[7]; ?>
+			</p>
+			<p class="tileRole">
+				<?php echo $data_array[6]; ?>
+			</p>
+		</div>
+		<div class="col-2" id="champ5" name="<?php echo $data_array[9]; ?>">
+		<p class="tileName">
+				<?php echo $data_array[9]; ?>
+			</p>
+			<p class="tileRole">
+				<?php echo $data_array[8]; ?>
+			</p>
+		</div>
 	</body>
 </html>
